b01b01c2dad56870b0d3a77ba5fd8983eee516ba
ABHIK GOGOI <f20250723@pilani.bits-pilani.ac.in>
2026-08-28 11:45:24 +0000

    lab01 task4

--- labs/lab01/task4/cla64_flat.v
+++ labs/lab01/task4/cla64_flat.v
@@ -22,7 +22,7 @@ module cla64_flat(
   generate
     for (i = 0; i < 64; i = i + 1) begin : gen_pg 
       xor #(2) (p[i], a[i], b[i]);
-      and #(2) (g[i], a[i], b[i]);
+      and #(2) (g[i], a[i], b[i]); 
     end
   endgenerate
 
